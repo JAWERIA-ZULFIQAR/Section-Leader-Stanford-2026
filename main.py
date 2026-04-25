e9c5d70cc2ca7a65ca78e1129de79af6937a64b4
JAWERIA-ZULFIQAR <jaweria4387@gmail.com>
2026-04-25 23:11:11 +0500

    Add files via upload

--- main.py
+++ main.py
@@ -4,25 +4,24 @@ solve the problem. You can click the blue show solution button on the left to se
 the answer if you get too stuck or want to check your work!
 """
 
-# This tells PyCharm who Karel is
-# Every Karel file has a line just like it
 from karel.stanfordkarel import *
 
-# this program executes in a special function called main
 def main():
+    """
+    Karel starts facing East in the bottom left corner of the world and ends facing East in the bottom right corner of the world.
+    """
     move()
     pick_beeper()
-    move()
     turn_left()
+    move()
     move()
     for i in range (3):
         turn_left()
-    move()
     put_beeper()
     move()
 
 
-# This is "boilerplate" code which launches your code
-# when you hit the run button
+# There is no need to edit code beyond this point
+
 if __name__ == '__main__':
     main()
